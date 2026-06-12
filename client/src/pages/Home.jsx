437ba6297b0fb28072e7e6dbc3149f5d159781f1
Hasini Panjala <hasinipanjala@Hasinis-MacBook-Air.local>
2026-06-12 11:34:27 +0530

    Clean App CSS

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,28 +1,65 @@
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
     <div className="home">
 
-      <div className="floating-circle circle1"></div>
-      <div className="floating-circle circle2"></div>
-      <div className="floating-circle circle3"></div>
+      <div className="bubble bubble1"></div>
+      <div className="bubble bubble2"></div>
+      <div className="bubble bubble3"></div>
+      <div className="bubble bubble4"></div>
+      <div className="bubble bubble5"></div>
 
       <div className="hero-content">
 
-        <h1>Level Up Your Life</h1>
+        <h1>HabitForge</h1>
 
-        <h2>
-          Build Habits. Earn XP. Stay Consistent.
-        </h2>
+        <h2>Build Better Habits</h2>
 
-        <p>
-          HabitForge transforms daily habits into
-          a rewarding journey of growth,
-          achievement, and consistency.
+        <p className="tagline">
+          Track habits, earn XP, maintain streaks and
+          <span className="level-up"> Level Up Your Life</span>
         </p>
 
-        <button>
-          Start Your Journey
-        </button>
+        <Link to="/register">
+          <button className="hero-btn">
+            Start Your Journey
+          </button>
+        </Link>
+
+        {/* FEATURES */}
+
+        <div className="features">
+
+          <div className="feature-card">
+            <h3>🎯 Smart Tracking</h3>
+            <p>
+              Track habits effortlessly every day.
+            </p>
+          </div>
+
+          <div className="feature-card">
+            <h3>🔥 Streak System</h3>
+            <p>
+              Build consistency through streaks.
+            </p>
+          </div>
+
+          <div className="feature-card">
+            <h3>⭐ XP Rewards</h3>
+            <p>
+              Earn XP whenever you complete habits.
+            </p>
+          </div>
+
+          <div className="feature-card">
+            <h3>🏆 Achievements</h3>
+            <p>
+              Unlock badges and milestones.
+            </p>
+          </div>
+
+        </div>
 
       </div>
 
